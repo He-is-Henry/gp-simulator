183e1014f9813761cd988418d51dc686f6093e42
He-is-Henry <henries90@gmail.com>
2025-05-14 00:12:42 +0100

    Initial upload

---
+++ scripts/script.js
@@ -0,0 +1,79 @@
+document.querySelectorAll("tr").forEach((row) => {
+  // console.log(row.dataset)
+});
+function addInputListeners() {
+  document.querySelectorAll("input").forEach((input) => {
+    const inputData = input.dataset;
+    input.removeEventListener("change", input._handler);
+    input.addEventListener("change", () => {
+      let creditPoint = 1;
+      document
+        .querySelectorAll(`[data-id="${inputData.id}"]`)
+        .forEach((element) => {
+          console.log(element.value);
+          creditPoint *= element.value;
+          document.querySelector(`.credit-point-${inputData.id}`).innerHTML =
+            creditPoint;
+        });
+      calclulateValues();
+    });
+  });
+}
+
+addInputListeners();
+
+function calclulateValues() {
+  let totalUnits = 0;
+  document.querySelectorAll(".unit").forEach((unit) => {
+    totalUnits += Number(unit.value);
+    document.querySelector(".tnu").innerHTML = totalUnits;
+  });
+  let totalPoints = 0;
+  document.querySelectorAll(".credit-point").forEach((unit) => {
+    totalPoints += Number(unit.innerHTML);
+    document.querySelector(".tcp").innerHTML = totalPoints;
+  });
+  const GP = totalPoints / totalUnits;
+  document.querySelector(".gp").innerHTML = GP.toFixed(2);
+}
+
+document.querySelector(".addBtn").addEventListener("click", () => {
+  const newRow = document.createElement("tr");
+  newRow.classList.add(document.querySelectorAll(".row").length + 1, "row");
+  newRow.innerHTML = ` 
+          <td><input type="text" name="" id="" value="Course ${
+            document.querySelectorAll(".row").length + 1
+          }" /></td>
+          <td>
+            <input
+              class="unit"
+              type="number"
+              name=""
+              id=""
+              value="2"
+              data-id="${document.querySelectorAll(".row").length + 1}"
+              max="5"
+              min="0"
+            />
+          </td>
+          <td>
+            <input
+              class="grade"
+              type="number"
+              name=""
+              id=""
+              value="5"
+              data-id="${document.querySelectorAll(".row").length + 1}"
+              max="5"
+              min="0"
+            />
+          </td>
+          <td class="credit-point-${
+            document.querySelectorAll(".row").length + 1
+          } credit-point">10</td>`;
+  document.querySelector("tbody").appendChild(newRow);
+  addInputListeners();
+  calclulateValues();
+});
+
+calclulateValues();
